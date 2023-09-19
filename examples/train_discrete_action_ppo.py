82cb45bfeb75706ee54b06618bf9defe5664ed95
James Liu <actions@users.noreply.github.com>
2023-09-19 10:33:25 -0700

    Move train discrete action ppo to examples directory

--- examples/train_discrete_action_ppo.py
+++ examples/train_discrete_action_ppo.py
@@ -5,16 +5,24 @@ from nanoppo.discrete_action_ppo import PPO
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+
 def main():
-    env = gym.make('CartPole-v1')
+    env = gym.make("CartPole-v1")
     action_dim = env.action_space.n
 
     state_dim = env.observation_space.shape[0]
-    print('action space', env.action_space, action_dim)
+    print("action space", env.action_space, action_dim)
     ppo = PPO(state_dim, action_dim)
 
     episode_rewards = []
-    states, actions, rewards, next_states, done_flags, log_probs = [], [], [], [], [], []
+    states, actions, rewards, next_states, done_flags, log_probs = (
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    )
     buffer_interval = 20
     print_every = 10
 
@@ -22,7 +30,14 @@ def main():
         state, info = env.reset()
         total_reward = 0
         if episode % buffer_interval == 0:
-            states, actions, rewards, next_states, done_flags, log_probs = [], [], [], [], [], []
+            states, actions, rewards, next_states, done_flags, log_probs = (
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+            )
         for step in range(1, 10000):
             state_tensor = torch.tensor(np.array([state]), dtype=torch.float32)
             action_prob = ppo.policy(state_tensor)
@@ -47,17 +62,20 @@ def main():
 
         if episode % print_every == 0:
             avg_reward = sum(episode_rewards[-print_every:]) / print_every
-            print(f"Episode {episode}: Average Reward over last {print_every} episodes: {avg_reward}")
+            print(
+                f"Episode {episode}: Average Reward over last {print_every} episodes: {avg_reward}"
+            )
 
     print("Initial rewards:", episode_rewards[:20])
     print("Final rewards:", episode_rewards[-20:])
 
     plt.plot(episode_rewards)
-    plt.xlabel('Episode')
-    plt.ylabel('Total Reward')
-    plt.title('PPO Training Rewards')
-    plt.savefig(f'ppo_rewards_{buffer_interval}.png')
+    plt.xlabel("Episode")
+    plt.ylabel("Total Reward")
+    plt.title("PPO Training Rewards")
+    plt.savefig(f"ppo_rewards_{buffer_interval}.png")
     plt.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
